68b90d7722d0d518ffc67cbbfe1831ff02cce292
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-10-29 22:23:54 +1000

    Don't generate source maps in production

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -50,7 +50,7 @@ module.exports = {
   bail: true,
   // We generate sourcemaps in production. This is slow but gives good results.
   // You can exclude the *.map files from the build during deployment.
-  devtool: 'source-map',
+  // devtool: 'source-map',
   // In production, we only want to load the polyfills and the app code.
   entry: [
     require.resolve('./polyfills'),
@@ -85,7 +85,6 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -102,7 +101,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: 'babel',
-        
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -156,7 +154,6 @@ module.exports = {
       }
     ]
   },
-  
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
