f9612e97215eaf7f036481f81ca6abc320021072
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-10-30 17:02:43 +1000

    Show contexts of a task in chips

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
 import ActionAssignment from 'material-ui/svg-icons/action/assignment';
 import ActionDone from 'material-ui/svg-icons/action/done';
 import { lightGreenA400 } from 'material-ui/styles/colors';
@@ -8,14 +9,19 @@ import OakPropTypes from '../PropTypes';
 
 const Item = ({ item, onAvatarTouchTap, id }) => (
   <ListItem
-    primaryText={item.name}
     leftAvatar={<Avatar
       color={item.isCompleted ? lightGreenA400 : undefined}
       onTouchTap={e => onAvatarTouchTap(e, id)}
       icon={item.isProject ? <ActionAssignment /> : <ActionDone />}
     />}
-
-  />
+  >
+    {item.name}
+    {item.isProject ? undefined :
+      <div className="chip-container">
+        {item.contexts.map(
+          (context, i) => <Chip key={i} style={{ marginRight: 5, marginTop: 3 }}>{context}</Chip>)}
+      </div>}
+  </ListItem>
 );
 
 Item.propTypes = {
