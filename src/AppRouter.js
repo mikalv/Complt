b099ae733b416cdc12ba94740f38dfc20f7e2b16
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-10-31 17:56:05 +1000

    Use browserHistory in Router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,12 +1,11 @@
-
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './App';
 import Home from './components/Home';
 
 
 const AppRouter = () => (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
     </Route>
