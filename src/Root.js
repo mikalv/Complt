1a415e418912eaf66af73663f4df448fcc0215d1
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-11-04 22:48:50 +1000

    Add redux-persist

--- src/Root.js
+++ src/Root.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import { Provider } from 'react-redux';
 import App from './App';
 import Home from './components/Home';
 import Inbox from './components/Inbox';
 import projects from './projects';
 
+
 const store = createStore(
   combineReducers({
     routing: routerReducer,
     projects,
-  }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  }),
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    autoRehydrate()
+  )
 );
+
+persistStore(store, {
+  blacklist: 'routing',
+});
 
 const history = syncHistoryWithStore(browserHistory, store);
 
