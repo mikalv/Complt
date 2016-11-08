a546f795ba9ecf527198eeef8b6bffabdc4b58b8
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-11-08 19:56:56 +1000

    Add tests for reducers

---
+++ src/redux/__tests__/drawer.js
@@ -0,0 +1,32 @@
+import reducer from '../drawer';
+import { toggleDrawer, dockDrawer } from '../actions';
+
+describe('drawerReducer', () => {
+  it('should return the inital state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      isDocked: false,
+      isOpen: false,
+    });
+  });
+  it('should handle TOGGLE_DRAWER when it is undocked', () => {
+    expect(reducer(undefined, toggleDrawer())).toEqual({
+      isDocked: false,
+      isOpen: true,
+    });
+  });
+  it('should handle TOGGLE_DRAWER when it is docked', () => {
+    expect(reducer({
+      isDocked: true,
+      isOpen: true,
+    }, toggleDrawer())).toEqual({
+      isDocked: true,
+      isOpen: true,
+    });
+  });
+  it('should handle DOCK_DRAWER', () => {
+    expect(reducer(undefined, dockDrawer(true))).toEqual({
+      isDocked: true,
+      isOpen: true,
+    });
+  });
+});
