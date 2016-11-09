1ca1187073cc5db7c539295a0f786bc200b9dd3c
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-11-09 17:46:15 +1000

    add injectTapEventPlugin to App component test

--- src/components/__tests__/Home.js
+++ src/components/__tests__/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Home from '../Home';
 import MuiTheme from '../../MuiTheme';
+
+injectTapEventPlugin();
 
 describe('Home component', () => {
   it('renders without crashing', () => {
