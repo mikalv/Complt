d910b816452a1b44b3bfe71645648034750f158f
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-11 08:50:58 +1000

    Change App component tests

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { App } from '../App';
+import MuiTheme from '../MuiTheme';
 
 describe('App component', () => {
   it('renders without crashing', () => {
-    renderer.create(<App drawer={{ isOpen: false, isDocked: false }} dockDrawer={() => {}} />);
+    renderer.create(<MuiTheme>
+      <App drawer={{ isOpen: false, isDocked: false }} dockDrawer={() => {}} />
+    </MuiTheme>);
   });
   it('calls dockDrawer function initally', () => {
     const dockDrawer = jest.fn();
-    renderer.create(<App drawer={{ isOpen: false, isDocked: false }} dockDrawer={dockDrawer} />);
+    renderer.create(<MuiTheme>
+      <App drawer={{ isOpen: false, isDocked: false }} dockDrawer={dockDrawer} />
+    </MuiTheme>);
     expect(dockDrawer).toBeCalled();
   });
   it('renders correctly when the drawer is open and undocked', () => {
-    const component = renderer.create(
-      <App drawer={{ isOpen: true, isDocked: false }} dockDrawer={() => {}} />);
+    const component = renderer.create(<MuiTheme>
+      <App drawer={{ isOpen: true, isDocked: false }} dockDrawer={() => {}} />
+    </MuiTheme>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('renders correctly when the drawer is closed and undocked', () => {
-    const component = renderer.create(
-      <App drawer={{ isOpen: false, isDocked: false }} dockDrawer={() => {}} />);
+    const component = renderer.create(<MuiTheme>
+      <App drawer={{ isOpen: false, isDocked: false }} dockDrawer={() => {}} />
+    </MuiTheme>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
   it('renders correctly when the drawer is docked', () => {
-    const component = renderer.create(
-      <App drawer={{ isOpen: true, isDocked: true }} dockDrawer={() => {}} />);
+    const component = renderer.create(<MuiTheme>
+      <App drawer={{ isOpen: true, isDocked: true }} dockDrawer={() => {}} />
+    </MuiTheme>);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
