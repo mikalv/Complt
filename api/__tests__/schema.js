74f2ce2fed932ca76668435933ee65a50b042c75
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-17 16:01:51 +1000

    Add useless test so all code is included in coverage reports

--- api/__tests__/schema.js
+++ api/__tests__/schema.js
@@ -1,5 +1,10 @@
-import '../schema';
+import schema from '../schema';
 import '../auth';
 import '../../handler';
 
-xdescribe('GraphQL schema');
+// Currently this test is only here so all code is included in coverage reports
+describe('GraphQL schema', () => {
+  it('is an object', () => {
+    expect(typeof schema).toEqual('object');
+  });
+});
