809d55c0df0dcd6a556e55d8cc6bf309c53299a8
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-17 14:44:24 +1000

    Add tests for getItemsByUserAndIds and make it reject if it is passed falsy arguments

--- api/lib/getItemsByUserAndIds.js
+++ api/lib/getItemsByUserAndIds.js
@@ -1,6 +1,7 @@
 import { Item } from '../dynamoModels';
 
-function getItemsByUserAndIds(user, ids = []) {
+function getItemsByUserAndIds(user, ids) {
+  if (!user && !ids) return Promise.reject('No user id or no item ids were given');
   const idsWithOwner = ids.map(id => ({
     id,
     owner: user,
