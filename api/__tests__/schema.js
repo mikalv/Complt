28b6c6f38bb6141bca72b19b11d225c1bf13ec2a
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-17 16:40:32 +1000

    Mock auth0 in test for api/schema

--- api/__tests__/schema.js
+++ api/__tests__/schema.js
@@ -2,6 +2,7 @@ import schema from '../schema';
 import '../auth';
 import '../../handler';
 
+jest.mock('auth0');
 // Currently this test is only here so all code is included in coverage reports
 describe('GraphQL schema', () => {
   it('is an object', () => {
