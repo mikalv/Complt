d6d4180ba02ee4d22d1835aed1db0d5cb852ef4a
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-19 00:04:46 +1000

    Add tests for verifyItemExists and make it reject if it is passed falsy arguments

--- api/lib/verifyItemExists.js
+++ api/lib/verifyItemExists.js
@@ -1,8 +1,9 @@
 import { Item, Inbox, Root } from '../dynamoModels';
 
 function verifyItemExists(user, itemId) {
+  if (!user || !itemId) return Promise.reject('No user id or no item id was given');
   return Item.getAsync({ owner: user, id: itemId }).then((item) => {
-    if (item === null || item === undefined) {
+    if (!item) {
       if (itemId === 'inbox') {
         return Inbox.createAsync({ owner: user }).then(inboxCreated => inboxCreated.attrs);
       }
