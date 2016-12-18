3b6d35c38e46b82fba9a092c7b8ab48dcc3a2e11
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-18 23:34:40 +1000

    Add additional test for addItemToProject

--- api/lib/__tests__/addItemToProject.js
+++ api/lib/__tests__/addItemToProject.js
@@ -8,8 +8,10 @@ Item.createAsync.mockImplementation(item =>
   Promise.resolve({ attrs: { ...item, id: 'someItemId' } }));
 Item.updateAsync.mockImplementation(item =>
   Promise.resolve({ attrs: item }));
-verifyItemExists.mockImplementation((user, id) =>
-  Promise.resolve({ owner: user, id, children: [] }));
+verifyItemExists.mockImplementation((user, id) => {
+  if (id === 'someProjectWithoutChildren') return Promise.resolve({ owner: user, id });
+  return Promise.resolve({ owner: user, id, children: [] });
+});
 
 describe('addItemToProject()', () => {
   it('rejects if it is called with no arguments', () =>
@@ -28,6 +30,11 @@ describe('addItemToProject()', () => {
   it('calls Item.updateAsync with the correct data', () =>
     addItemToProject({ owner: 'someUserId', name: 'a task' }, 'someProjectId').then(() => {
       expect(Item.updateAsync).toBeCalledWith({ owner: 'someUserId', id: 'someProjectId', children: ['someItemId'] });
+    })
+  );
+  it('calls Item.updateAsync with the correct data if the project did not have any children', () =>
+    addItemToProject({ owner: 'someUserId', name: 'a task' }, 'someProjectWithoutChildren').then(() => {
+      expect(Item.updateAsync).toBeCalledWith({ owner: 'someUserId', id: 'someProjectWithoutChildren', children: ['someItemId'] });
     })
   );
   it('resolves with the correct item', () =>
