e4441521e3a527bf1058b2725ced5f55d2b8ff42
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-18 13:30:30 +1000

    Add tests for addItemToProject and make it reject if it is passed falsy arguments

---
+++ api/lib/__tests__/addItemToProject.js
@@ -0,0 +1,38 @@
+import addItemToProject from '../addItemToProject';
+import { Item } from '../../dynamoModels';
+import verifyItemExists from '../verifyItemExists';
+
+jest.mock('../../dynamoModels').mock('../verifyItemExists');
+
+Item.createAsync.mockImplementation(item =>
+  Promise.resolve({ attrs: { ...item, id: 'someItemId' } }));
+Item.updateAsync.mockImplementation(item =>
+  Promise.resolve({ attrs: item }));
+verifyItemExists.mockImplementation((user, id) =>
+  Promise.resolve({ owner: user, id, children: [] }));
+
+describe('addItemToProject()', () => {
+  it('rejects if it is called with no arguments', () =>
+    addItemToProject().catch(error => expect(error).toEqual('No item or no project id was given'))
+  );
+  it('calls verifyItemExists with the correct data', () =>
+    addItemToProject({ owner: 'someUserId' }, 'someProjectId').then(() => {
+      expect(verifyItemExists).toBeCalledWith('someUserId', 'someProjectId');
+    })
+  );
+  it('calls Item.createAsync with the correct data', () =>
+    addItemToProject({ owner: 'someUserId', name: 'a task' }, 'someProjectId').then(() => {
+      expect(Item.createAsync).toBeCalledWith({ owner: 'someUserId', name: 'a task' });
+    })
+  );
+  it('calls Item.updateAsync with the correct data', () =>
+    addItemToProject({ owner: 'someUserId', name: 'a task' }, 'someProjectId').then(() => {
+      expect(Item.updateAsync).toBeCalledWith({ owner: 'someUserId', id: 'someProjectId', children: ['someItemId'] });
+    })
+  );
+  it('resolves with the correct item', () =>
+    addItemToProject({ owner: 'someUserId', name: 'a task' }, 'someProjectId').then((item) => {
+      expect(item).toEqual({ owner: 'someUserId', name: 'a task', id: 'someItemId' });
+    })
+  );
+});
--- api/lib/addItemToProject.js
+++ api/lib/addItemToProject.js
@@ -2,6 +2,7 @@ import { Item } from '../dynamoModels';
 import verifyItemExists from './verifyItemExists';
 
 export default function addItemToProject(itemToAdd, projectId) {
+  if (!itemToAdd || !projectId) return Promise.reject('No item or no project id was given');
   return verifyItemExists(itemToAdd.owner, projectId).then(project =>
     Item.createAsync(itemToAdd).then(itemCreated =>
       Item.updateAsync({
