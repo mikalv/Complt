3fdfe11d6fcde46ff6011d6f58e4a216c64120d5
mitchellhamilton <mitchell@mitchellhamilton.me>
2016-12-19 13:27:22 +1000

    Add snapshot test of GraphQL schema

--- api/__tests__/schema.js
+++ api/__tests__/schema.js
@@ -1,11 +1,9 @@
+import { printSchema } from 'graphql';
 import schema from '../schema';
-import '../auth';
 import '../../handler';
 
-jest.mock('auth0');
-// Currently this test is only here so all code is included in coverage reports
 describe('GraphQL schema', () => {
-  it('is an object', () => {
-    expect(typeof schema).toEqual('object');
+  it('matches the snapshot', () => {
+    expect(printSchema(schema)).toMatchSnapshot();
   });
 });
