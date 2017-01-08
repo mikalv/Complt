29d341341bc39f2c5f2f7404baee0e1781780026
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-08 14:07:22 +1000

    Revert "Make the Apollo middleware get the JWT directly from the redux store"
    This reverts commit 19c124c0d62bfc2da26c544a9f51ced435e5aa4e.

--- src/Root.js
+++ src/Root.js
@@ -20,7 +20,7 @@ const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_API
 networkInterface.use([{
   applyMiddleware(req, next) {
     if (!req.options.headers) req.options.headers = {};
-    const token = store.getState().auth; // eslint-disable-line no-use-before-define
+    const token = JSON.parse(localStorage.getItem('reduxPersist:auth')) || null;
     req.options.headers.authorization = token ? `Bearer ${token}` : null;
     next();
   },
