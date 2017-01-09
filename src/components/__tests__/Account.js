5805b6a0df7af3067a0d49eebbc0d6835ac39fe2
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-09 18:40:09 +1000

    Fix linting error

--- src/components/__tests__/Account.js
+++ src/components/__tests__/Account.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Account } from '../Account';
-import MuiTheme from '../../MuiTheme';
 
 describe('Account component', () => {
   it('renders without crashing', () => {
