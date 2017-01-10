d10e091044690a29ea5b7b86a0da03c5113d92bd
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-10 16:59:28 +1000

    Remove Serverless, GraphQL and the api

--- api/authClient.js
+++
@@ -1,8 +0,0 @@
-import { AuthenticationClient } from 'auth0';
-
-const authenticationClient = new AuthenticationClient({
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
-});
-
-export default authenticationClient;
--- scripts/start.js
+++ scripts/start.js
@@ -2,12 +2,6 @@ process.env.NODE_ENV = 'development';
 process.env.REACT_APP_AUTH0_DOMAIN = 'oakapp.auth0.com';
 process.env.REACT_APP_AUTH0_CLIENT_ID = 'GfMoEkzkCGYB9p1cyQ042XyVshskXt8p';
 
-if (process.env.PROD_API === 'true') {
-  process.env.REACT_APP_API_URL = 'https://5yt9wld04i.execute-api.us-east-1.amazonaws.com/production/graphql';
-} else {
-  process.env.REACT_APP_API_URL = 'https://8lq0gtonr5.execute-api.us-east-1.amazonaws.com/dev/graphql';
-}
-
 // Load environment variables from .env file. Surpress warnings using silent
 // if this file is missing. dotenv will never modify any environment variables
 // that have already been set.
