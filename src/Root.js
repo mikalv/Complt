94147a22e517ad76f8c48061980bb754cfbcd59e
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-11 16:16:56 +1000

    Remove items reducer

--- src/Root.js
+++ src/Root.js
@@ -1,49 +1,32 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { persistStore, autoRehydrate } from 'redux-persist';
-import PouchMiddleware from 'pouch-redux-middleware';
 import auth from './redux/auth';
-import items from './redux/items';
 import OakRouter from './OakRouter';
-import db from './db';
-import { DELETE_ITEM_POUCH, INSERT_ITEM_POUCH, UPDATE_ITEM_POUCH } from './redux/actionTypes';
 
 if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   navigator.serviceWorker.register('/service-worker.js');
 }
-
-const pouchMiddleware = PouchMiddleware({ // eslint-disable-line new-cap
-  path: '/items',
-  db,
-  actions: {
-    remove: doc => ({ type: DELETE_ITEM_POUCH, id: doc._id }),
-    insert: doc => ({ type: INSERT_ITEM_POUCH, item: doc }),
-    update: doc => ({ type: UPDATE_ITEM_POUCH, item: doc }),
-  },
-});
 
 let enhancer;
 
 if (window.__REDUX_DEVTOOLS_EXTENSION__) {
   enhancer = compose(
       autoRehydrate(),
-      applyMiddleware(pouchMiddleware),
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 } else {
   enhancer = compose(
-    autoRehydrate(),
-    applyMiddleware(pouchMiddleware)
+    autoRehydrate()
     );
 }
 
 const store = createStore(
   combineReducers({
     routing: routerReducer,
-    items,
     auth,
   }),
   enhancer
