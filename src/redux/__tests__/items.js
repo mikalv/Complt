2700ec18f7a002fc5ee935f6dcfd81da6849255b
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-13 13:27:35 +1000

    Add tests for CREATE_ITEM action in items reducer

--- src/redux/__tests__/items.js
+++ src/redux/__tests__/items.js
@@ -1,5 +1,10 @@
+import uuid from 'uuid';
 import reducer, { initialState } from '../items';
+import * as actions from '../actions';
 import { DELETE_ITEM_POUCH, INSERT_ITEM_POUCH, UPDATE_ITEM_POUCH } from '../actionTypes';
+
+jest.mock('uuid');
+uuid.mockReturnValue('item5');
 
 const items = [
   { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
@@ -7,6 +12,8 @@ const items = [
   { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
   { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
 ];
+const itemsWithRoot = [...items, { _id: 'root', isProject: true, children: [] }];
+const itemsWithInbox = [...items, { _id: 'inbox', isProject: true, children: [] }];
 
 describe('itemsReducer', () => {
   it('returns the inital state', () => {
@@ -34,6 +41,64 @@ describe('itemsReducer', () => {
       { _id: 'item2', isProject: false, isCompleted: false, tags: [] },
       { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
       { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
+    ]);
+  });
+  it('handles CREATE_ITEM with a task correctly', () => {
+    expect(reducer(items, actions.createTask('item4', { isProject: false, isCompleted: false, tags: [], name: 'A Task' }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3', 'item5'] },
+      { _id: 'item5', isProject: false, isCompleted: false, tags: [], name: 'A Task' },
+    ]);
+  });
+  it('handles CREATE_ITEM with a project correctly', () => {
+    expect(reducer(items, actions.createProject('item4', { isProject: true, name: 'A Project', children: [] }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3', 'item5'] },
+      { _id: 'item5', isProject: true, name: 'A Project', children: [] },
+    ]);
+  });
+  it('handles CREATE_ITEM correctly when the parent is root and it doesn\'t exist', () => {
+    expect(reducer(items, actions.createProject('root', { isProject: false, isCompleted: false, tags: [], name: 'A Task' }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
+      { _id: 'root', isProject: true, children: ['item5'] },
+      { _id: 'item5', isProject: false, isCompleted: false, tags: [], name: 'A Task' },
+    ]);
+  });
+  it('handles CREATE_ITEM correctly when the parent is root and it does exist', () => {
+    expect(reducer(itemsWithRoot, actions.createProject('root', { isProject: false, isCompleted: false, tags: [], name: 'A Task' }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
+      { _id: 'root', isProject: true, children: ['item5'] },
+      { _id: 'item5', isProject: false, isCompleted: false, tags: [], name: 'A Task' },
+    ]);
+  });
+  it('handles CREATE_ITEM correctly when the parent is inbox and it doesn\'t exist', () => {
+    expect(reducer(items, actions.createProject('inbox', { isProject: false, isCompleted: false, tags: [], name: 'A Task' }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
+      { _id: 'inbox', isProject: true, children: ['item5'] },
+      { _id: 'item5', isProject: false, isCompleted: false, tags: [], name: 'A Task' },
+    ]);
+  });
+  it('handles CREATE_ITEM correctly when the parent is inbox and it does exist', () => {
+    expect(reducer(itemsWithInbox, actions.createProject('inbox', { isProject: false, isCompleted: false, tags: [], name: 'A Task' }))).toEqual([
+      { _id: 'item1', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item2', isProject: false, isCompleted: true, tags: [] },
+      { _id: 'item3', isProject: false, isCompleted: false, tags: [] },
+      { _id: 'item4', isProject: true, children: ['item1', 'item2', 'item3'] },
+      { _id: 'inbox', isProject: true, children: ['item5'] },
+      { _id: 'item5', isProject: false, isCompleted: false, tags: [], name: 'A Task' },
     ]);
   });
 });
