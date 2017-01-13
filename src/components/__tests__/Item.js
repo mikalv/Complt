738450bb41a5782a35223612ca05d1171e53d5b0
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-13 11:21:46 +1000

    Fix Item and ItemList component tests

--- src/components/__tests__/Item.js
+++ src/components/__tests__/Item.js
@@ -4,29 +4,29 @@ import Item from '../Item';
 
 describe('Item component', () => {
   it('renders an uncompleted task correctly', () => {
-    const component = mount(<Item item={{ name: 'Task', __typename: 'Task', isCompleted: false }} />);
+    const component = mount(<Item item={{ name: 'Task', isProject: false, isCompleted: false }} />);
     expect(component).toMatchSnapshot();
   });
   it('renders a completed task correctly', () => {
-    const component = mount(<Item item={{ name: 'Task', __typename: 'Task', isCompleted: true }} />);
+    const component = mount(<Item item={{ name: 'Task', isProject: false, isCompleted: true }} />);
     expect(component).toMatchSnapshot();
   });
   it('renders a task with tags correctly', () => {
-    const component = mount(<Item item={{ name: 'Task', __typename: 'Task', isCompleted: false, tags: ['@tag', '@another-tag'] }} />);
+    const component = mount(<Item item={{ name: 'Task', isProject: false, isCompleted: false, tags: ['@tag', '@another-tag'] }} />);
     expect(component).toMatchSnapshot();
   });
   it('renders a project correctly', () => {
-    const component = mount(<Item item={{ name: 'Project', __typename: 'Project' }} />);
+    const component = mount(<Item item={{ name: 'Project', isProject: true }} />);
     expect(component).toMatchSnapshot();
   });
   it('renders the delete button when it has the canDelete prop', () => {
-    const component = mount(<Item canDelete item={{ name: 'Task', __typename: 'Task', isCompleted: false }} />);
+    const component = mount(<Item canDelete item={{ name: 'Task', isProject: false, isCompleted: false }} />);
     expect(component).toMatchSnapshot();
   });
   it('only calls onDelete when the delete button is pressed', () => {
     const onItemTap = jest.fn();
     const onDelete = jest.fn();
-    const component = mount(<Item canDelete onItemTap={onItemTap} onDelete={onDelete} item={{ name: 'Task', __typename: 'Task', isCompleted: false }} />);
+    const component = mount(<Item canDelete onItemTap={onItemTap} onDelete={onDelete} item={{ name: 'Task', isProject: false, isCompleted: false }} />);
     component.find('Button').simulate('click');
     expect(onItemTap).not.toBeCalled();
     expect(onDelete).toBeCalled();
