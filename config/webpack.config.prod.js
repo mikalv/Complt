c0d3da6d0d333284adc4788c771cd693b575f93e
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-15 16:05:22 +1000

    Change the source map name

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -65,6 +65,7 @@ var config = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     filename: 'static/js/[name].[chunkhash:8].js',
+    sourceMapFilename: 'static/js/[name].[chunkhash:8].map.js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath
