61acab54b5470e56239a0c1ad9bfa51bea7a78a6
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-15 11:26:00 +1000

    Add more tests for AddItem

--- src/components/__tests__/AddItem.js
+++ src/components/__tests__/AddItem.js
@@ -6,4 +6,58 @@ describe('AddItem component', () => {
   it('renders without crashing', () => {
     mount(<AddItem onAddItem={jest.fn()} />);
   });
+  it('renders correctly when initialIsProject is false', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} />);
+    expect(component).toMatchSnapshot();
+  });
+  it('renders correctly when initialIsProject is true', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} initialIsProject />);
+    expect(component).toMatchSnapshot();
+  });
+  it('renders correctly when canChangeType is true and initialIsProject is false', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} canChangeType />);
+    expect(component).toMatchSnapshot();
+  });
+  it('renders correctly when canChangeType is true and initialIsProject is true', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} canChangeType initialIsProject />);
+    expect(component).toMatchSnapshot();
+  });
+  it('correctly changes between types when the button is clicked', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} canChangeType />);
+    const changeTypeButton = component.find({ children: 'assignment' });
+    expect(changeTypeButton.text()).toEqual('assignment');
+    changeTypeButton.simulate('click');
+    expect(changeTypeButton.text()).toEqual('done');
+  });
+  it('correctly updates the value when the input changes', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} canChangeType />);
+    const TextField = component.find('TextField');
+    const input = component.find('input').first();
+    input.simulate('change', { target: { value: 'Task @tag' } });
+    expect(TextField.props().value).toEqual('Task @tag');
+  });
+  it('correctly adds a @ when the tag button is clicked', () => {
+    const component = mount(<AddItem onAddItem={jest.fn()} canChangeType />);
+    const TextField = component.find('TextField');
+    const input = component.find('input').first();
+    input.simulate('change', { target: { value: 'Task' } });
+    component.find({ children: 'label' }).simulate('click');
+    expect(TextField.props().value).toEqual('Task @');
+  });
+  it('correctly calls onAddItem when the form is submitted', () => {
+    const onAddItem = jest.fn();
+    const component = mount(<AddItem onAddItem={onAddItem} canChangeType />);
+    const input = component.find('input').first();
+    input.simulate('change', { target: { value: 'Task @tag' } });
+    component.find({ type: 'submit' }).simulate('submit');
+    expect(onAddItem).toBeCalledWith({ isProject: false, tags: ['@tag'], name: 'Task', isCompleted: false });
+  });
+  it('does\'t call AddItem if the item returned from processItem is falsy', () => {
+    const onAddItem = jest.fn();
+    const component = mount(<AddItem onAddItem={onAddItem} canChangeType />);
+    const input = component.find('input').first();
+    input.simulate('change', { target: { value: '@tag' } });
+    component.find({ type: 'submit' }).simulate('submit');
+    expect(onAddItem).not.toBeCalled();
+  });
 });
