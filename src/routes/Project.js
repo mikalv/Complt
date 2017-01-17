fe8ca8b3fae2029ce14dacd5a576c3b1f57dbb62
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-18 09:39:38 +1000

    Move functionality in Project and RootProject to Projects

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import mapDispatchToProps from '../utils/mapDispatchToProps';
 import Projects from '../components/Projects';
-import OakPropTypes from '../PropTypes';
 
 export const Project = props => (
   <Projects
-    onCreateProject={project => props.createProject(props.routeParams.projectId, project)}
-    onCreateTask={task => props.createTask(props.routeParams.projectId, task)}
-    onDelete={(index) => {
-      const item = props.project[index];
-      if (item.isProject) props.deleteProject(props.routeParams.projectId, item._id);
-      else props.deleteTask(props.routeParams.projectId, item._id);
-    }}
-    onAvatarTap={(index) => {
-      if (!props.project[index].isProject) {
-        props.completeTask(props.project[index]._id, !props.project[index].isCompleted);
-      }
-    }}
-    onItemTap={(i) => {
-      if (props.project[i].isProject) {
-        props.router.push(`/project/${props.project[i]._id}`);
-      }
-    }}
-    projectChildren={props.project}
+    initialIsProject
+    canChangeType
+    projectId={props.routeParams.projectId}
+    routerPush={props.router.push}
   />
 );
 Project.propTypes = {
-  project: React.PropTypes.arrayOf(OakPropTypes.item),
-  createProject: React.PropTypes.func, // eslint-disable-line react/no-unused-prop-types
-  createTask: React.PropTypes.func, // eslint-disable-line react/no-unused-prop-types
-  deleteTask: React.PropTypes.func, // eslint-disable-line react/no-unused-prop-types
-  completeTask: React.PropTypes.func, // eslint-disable-line react/no-unused-prop-types
   routeParams: React.PropTypes.shape({
     projectId: React.PropTypes.string, // eslint-disable-line react/no-unused-prop-types
   }),
@@ -39,12 +17,5 @@ Project.propTypes = {
     push: React.PropTypes.func,
   }),
 };
-function mapStateToProps(state, ownProps) {
-  const project = state.items.find(item => item._id === ownProps.routeParams.projectId);
-  if (project === undefined) return { project: [] };
-  const projectChildren = project.children.map(id =>
-    state.items.find(item => item._id === id));
-  return { project: projectChildren };
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Project);
+export default Project;
