24a638ca4dda4402e79c5b126cc5f39ce5422054
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-18 11:24:46 +1000

    Move store configuration to src/redux/configureStore

--- src/Root.js
+++ src/Root.js
@@ -1,77 +1,7 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
-import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { persistStore, autoRehydrate } from 'redux-persist';
-import PouchMiddleware from 'pouch-redux-middleware';
-import RavenMiddleware from 'redux-raven-middleware';
-import thunk from 'redux-thunk';
-import auth from './redux/auth';
-import items from './redux/items';
-import profile from './redux/profile';
-import toasts from './redux/toasts';
-import syncState from './redux/syncState';
+import store, { history } from './redux/configureStore';
 import OakRouter from './OakRouter';
-import logException from './utils/logException';
-import db from './db';
-import { DELETE_ITEM_POUCH, INSERT_ITEM_POUCH, UPDATE_ITEM_POUCH } from './redux/actionTypes';
-
-global.db = db;
-
-const pouchMiddleware = PouchMiddleware({ // eslint-disable-line new-cap
-  path: '/items',
-  db,
-  actions: {
-    remove: doc => ({ type: DELETE_ITEM_POUCH, id: doc._id }),
-    insert: doc => ({ type: INSERT_ITEM_POUCH, item: doc }),
-    update: doc => ({ type: UPDATE_ITEM_POUCH, item: doc }),
-  },
-  handleResponse: (error, data, cb) => {
-    if (error) {
-      logException(new Error('An error occured in pouch-redux-middleware', { error, data }));
-    }
-    cb(error);
-  },
-});
-
-let middleware = applyMiddleware(thunk, pouchMiddleware, routerMiddleware(browserHistory));
-
-if (process.env.NODE_ENV === 'production') {
-  middleware = applyMiddleware(RavenMiddleware('https://36b5c3acd9014402a6a37623aef60814@sentry.io/118415', { release: process.env.REACT_APP_GIT_REF }), thunk, pouchMiddleware, routerMiddleware(browserHistory)); // eslint-disable-line new-cap
-}
-
-let enhancer;
-
-if (window.__REDUX_DEVTOOLS_EXTENSION__) {
-  enhancer = compose(
-      autoRehydrate(),
-      middleware,
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-} else {
-  enhancer = compose(
-    autoRehydrate(),
-    middleware
-    );
-}
-
-const store = createStore(
-  combineReducers({
-    routing: routerReducer,
-    items,
-    auth,
-    profile,
-    toasts,
-    syncState,
-  }),
-  enhancer
-);
-
-persistStore(store, {
-  whitelist: ['auth', 'profile'],
-});
-const history = syncHistoryWithStore(browserHistory, store);
 
 const Root = (
   <Provider store={store}>
