f9671391b1a120f664dd9e81b758894a709e9eb5
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-21 21:57:27 +1000

    Add src/utils/itemsToTree

---
+++ src/utils/itemsToTree.js
@@ -0,0 +1,17 @@
+function itemsToTree(items, topLevelId) {
+  const topLevelItem = items.find(item => item._id === topLevelId);
+  const topLevelItemChildren = topLevelItem.children.map((childId) => {
+    const item = items.find(itemInFind => itemInFind._id === childId);
+    if (item === undefined) return null;
+    if (item.isProject) {
+      return itemsToTree(items, item._id);
+    }
+    return item;
+  });
+  return {
+    ...topLevelItem,
+    children: topLevelItemChildren,
+  };
+}
+
+export default itemsToTree;
