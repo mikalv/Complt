124dd9680209be8217811a99af6c427d7acaedac
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-21 10:46:25 +1000

    Only install the offline-plugin runtime when the application is not running in electron

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Root from './Root';
 import './index.css';
 import './index.scss';
 
-offlinePlugin.install();
+if (!process.env.REACT_APP_ELECTRON) offlinePlugin.install();
 
 Raven.config('https://36b5c3acd9014402a6a37623aef60814@sentry.io/118415', { release: process.env.REACT_APP_GIT_REF }).install();
 
