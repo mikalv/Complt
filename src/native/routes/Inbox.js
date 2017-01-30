012a7d9c95d890d3b9269108b9a800b35d4bb3af
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-01-30 22:18:04 +1000

    Add native AddItem component

--- src/native/routes/Inbox.js
+++ src/native/routes/Inbox.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Text,
+  View,
+  StyleSheet,
 } from 'react-native';
 import { MaterialIcons } from 'react-native-vector-icons';
+import AddItem from '../components/AddItem';
 
 class Inbox extends Component {
   static navigationOptions = {
@@ -19,8 +21,25 @@ class Inbox extends Component {
     }),
   }
   render() {
-    return <Text>Thing</Text>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.big} />
+        <AddItem style={styles.small} />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  big: {
+    flex: 3,
+  },
+  small: {
+    flex: 1,
+  },
+});
 
 export default Inbox;
