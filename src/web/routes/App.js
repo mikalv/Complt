d458d6e3f30934c5bd6142ac8d7cb6e49329537a
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-18 16:26:51 +1000

    Change the sync button to a spinner when it is syncing
    Closes #82

--- src/web/routes/App.js
+++ src/web/routes/App.js
@@ -11,6 +11,7 @@ import SelectField from 'react-md/lib/SelectFields';
 import Snackbar from 'react-md/lib/Snackbars';
 import Link from 'react-router/lib/Link';
 import Button from 'react-md/lib/Buttons';
+import CircularProgress from 'react-md/lib/Progress/CircularProgress';
 import { connect } from 'react-redux';
 import { values as itemsToShowValues } from '../../common/redux/itemsToShow';
 import mapDispatchToProps from '../../common/utils/mapDispatchToProps';
@@ -72,7 +73,7 @@ export const App = props => (
         onChange={props.changeItemsToShow}
       />}
     toolbarActions={[
-      <Button icon onClick={props.attemptSync}><Sync /></Button>,
+      props.syncState.syncing ? <div style={{ margin: '10px' }}><CircularProgress id="syncing-spinner" /></div> : <Button icon onClick={props.attemptSync}><Sync /></Button>,
     ]}
   >
     {props.children}
@@ -91,6 +92,9 @@ App.propTypes = {
   attemptSync: React.PropTypes.func,
   itemsToShow: React.PropTypes.string,
   changeItemsToShow: React.PropTypes.func,
+  syncState: React.PropTypes.shape({
+    syncing: React.PropTypes.bool.isRequired,
+  }),
   /* eslint-disable react/no-unused-prop-types */
   toasts: React.PropTypes.arrayOf(React.PropTypes.shape({
     text: React.PropTypes.string.isRequired,
@@ -113,8 +117,8 @@ App.propTypes = {
   /* eslint-enable react/no-unused-prop-types */
 };
 
-function mapStateToProps({ toasts, dialogs, itemsToShow }) {
-  return { toasts, dialogs, itemsToShow };
+function mapStateToProps({ toasts, dialogs, itemsToShow, syncState }) {
+  return { toasts, dialogs, itemsToShow, syncState };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
