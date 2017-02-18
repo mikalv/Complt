3a601fe2943b6a1d463492d8cc28e22a12829e57
Mitchell Hamilton <mitchell@mitchellhamilton.me>
2017-02-18 09:30:39 +0000

    Revert "Change PouchDB import"
    This reverts commit 86aee4d4d6fa690393f97e72ffd3f8e6685ab0a4

--- src/common/PouchDB.js
+++ src/common/PouchDB.js
@@ -1,13 +1,3 @@
-import PouchDB from 'pouchdb-core/lib/index-browser.es';
-import PouchDBAdapterIdb from 'pouchdb-adapter-idb/lib/index.es';
-import PouchDBAdapterWebSql from 'pouchdb-adapter-websql/lib/index.es';
-import PouchDBAdapterHttp from 'pouchdb-adapter-http/lib/index.es';
-import PouchDBReplication from 'pouchdb-replication/lib/index.es';
-
-PouchDB
-.plugin(PouchDBAdapterIdb)
-.plugin(PouchDBAdapterWebSql)
-.plugin(PouchDBAdapterHttp)
-.plugin(PouchDBReplication);
+import PouchDB from 'pouchdb/lib/index-browser';
 
 export default PouchDB;
