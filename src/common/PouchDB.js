86aee4d4d6fa690393f97e72ffd3f8e6685ab0a4
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-18 17:24:17 +1000

    Change PouchDB import

--- src/common/PouchDB.js
+++ src/common/PouchDB.js
@@ -1,3 +1,13 @@
-import PouchDB from 'pouchdb/lib/index-browser';
+import PouchDB from 'pouchdb-core/lib/index-browser.es';
+import PouchDBAdapterIdb from 'pouchdb-adapter-idb/lib/index.es';
+import PouchDBAdapterWebSql from 'pouchdb-adapter-websql/lib/index.es';
+import PouchDBAdapterHttp from 'pouchdb-adapter-http/lib/index.es';
+import PouchDBReplication from 'pouchdb-replication/lib/index.es';
+
+PouchDB
+.plugin(PouchDBAdapterIdb)
+.plugin(PouchDBAdapterWebSql)
+.plugin(PouchDBAdapterHttp)
+.plugin(PouchDBReplication);
 
 export default PouchDB;
