00b1bdfddcdb4aa806933f059eb2b4572bd76897
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-21 18:00:40 +1000

    Persist itemsToShow reducer to localStorage
    Closes #88

--- src/web/configureStore.js
+++ src/web/configureStore.js
@@ -70,7 +70,7 @@ const store = createStore(
 );
 
 persistStore(store, {
-  whitelist: ['auth', 'profile'],
+  whitelist: ['auth', 'profile', 'itemsToShow'],
 });
 const history = syncHistoryWithStore(browserHistory, store);
 
