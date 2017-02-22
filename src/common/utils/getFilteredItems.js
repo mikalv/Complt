ae242dcede1433b3a19a405d2df6c30e40f1e293
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-22 21:30:59 +1000

    Fix error in src/common/utils/getFilteredItems

--- src/common/utils/getFilteredItems.js
+++ src/common/utils/getFilteredItems.js
@@ -3,9 +3,9 @@ import { SHOW_COMPLETED, SHOW_NOT_COMPLETED } from '../redux/itemsToShow';
 export default function getFilteredItems(items, itemsToShow) {
   switch (itemsToShow) {
     case SHOW_COMPLETED:
-      return items.filter(item => item.isCompleted);
+      return items.filter(item => item && item.isCompleted);
     case SHOW_NOT_COMPLETED:
-      return items.filter(item => !item.isCompleted);
+      return items.filter(item => item && !item.isCompleted);
     default:
       return items;
   }
