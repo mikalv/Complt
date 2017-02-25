13daa128e61e97f3679701579151e62e163ee49d
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-25 14:59:45 +1000

    Make All component and Tag component use NonProjectItemList component

--- src/web/routes/All.js
+++ src/web/routes/All.js
@@ -1,31 +1,10 @@
-import React from 'react';
 import { connect } from 'react-redux';
+import NonProjectItemList from '../components/NonProjectItemList';
 import mapDispatchToProps from '../../common/utils/mapDispatchToProps';
-import ItemList from '../components/ItemList';
 import getFilteredItems from '../../common/utils/getFilteredItems';
-import PropTypes from '../../common/PropTypes';
-
-export const All = props => (<ItemList
-  onItemAvatarTap={i =>
-      props.completeItem(props.projectChildren[i]._id, !props.projectChildren[i].isCompleted)}
-  onItemUpdate={i => props.showUpdateItemDialog(props.projectChildren[i]._id)}
-  items={props.projectChildren}
-  onItemTap={(i) => {
-    if (props.projectChildren[i].isProject) {
-      props.router.push(`/project/${props.projectChildren[i]._id}`);
-    }
-  }}
-/>);
-
-All.propTypes = {
-  projectChildren: React.PropTypes.arrayOf(PropTypes.item),
-  router: React.PropTypes.shape({
-    push: React.PropTypes.func,
-  }),
-};
 
 export function mapStateToProps(state) {
-  return { projectChildren: getFilteredItems(state.items.filter(({ _id }) => _id !== 'inbox' && _id !== 'root'), state.itemsToShow) };
+  return { items: getFilteredItems(state.items.filter(({ _id }) => _id !== 'inbox' && _id !== 'root'), state.itemsToShow) };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(All);
+export default connect(mapStateToProps, mapDispatchToProps)(NonProjectItemList);
--- src/web/routes/__tests__/All.js
+++ src/web/routes/__tests__/All.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { All, mapStateToProps } from '../All';
+import { mapStateToProps } from '../All';
 
 const task = { name: 'Task', isProject: false, isCompleted: false, tags: ['@tag'] };
 const project = { name: 'Project', isProject: true };
@@ -9,6 +7,7 @@ const items = [
   { ...task, _id: 'item1' },
   { ...project, _id: 'item2' },
   { ...task, _id: 'item3' },
+
 ];
 
 const itemsWithRootAndInbox = [
@@ -17,40 +16,8 @@ const itemsWithRootAndInbox = [
   { ...project, _id: 'inbox' },
 ];
 
-describe('All component', () => {
-  it('renders without crashing', () => {
-    mount(<All />);
-  });
-  it('renders correctly', () => {
-    const component = mount(<All projectChildren={items} />);
-    expect(component).toMatchSnapshot();
-  });
-  it('calls completeItem correctly when an item is completed', () => {
-    const completeItem = jest.fn();
-    const component = mount(<All projectChildren={items} completeItem={completeItem} />);
-    component.childAt(2).find('Avatar').simulate('click');
-    expect(completeItem).toBeCalledWith('item3', true);
-  });
-  it('calls routerPush correctly when a project is clicked', () => {
-    const routerPush = jest.fn();
-    const component = mount(<All projectChildren={items} router={{ push: routerPush }} />);
-    component.childAt(1).find('li').find('AccessibleFakeButtonInked')
-    .simulate('click');
-    expect(routerPush).toBeCalledWith('/project/item2');
-  });
-  it('calls showUpdateItemDialog when an item is updated', () => {
-    const showUpdateItemDialog = jest.fn();
-    const component = mount(<All
-      projectChildren={items}
-      showUpdateItemDialog={showUpdateItemDialog}
-    />);
-    component.childAt(2).find('MdCreate').simulate('click');
-    expect(showUpdateItemDialog).toBeCalledWith('item3');
-  });
-});
-
 describe('mapStateToProps', () => {
   it('returns an object with a projectChildren property which is an array of items without root or inbox', () => {
-    expect(mapStateToProps({ items: itemsWithRootAndInbox })).toEqual({ projectChildren: items });
+    expect(mapStateToProps({ items: itemsWithRootAndInbox })).toEqual({ items });
   });
 });
