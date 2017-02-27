5399346d76899cacf46a996c78c6a910be5e4811
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-02-27 21:59:32 +1000

    Add another e2e test and update e2e/.eslintrc.js

--- e2e/test.js
+++ e2e/test.js
@@ -6,4 +6,18 @@ describe('app', function () {
     expect(browser.getUrl()).to.equal('http://localhost:8080/inbox');
     expect(browser.$('.md-list--drawer').$$('li')[0].getText()).to.equal('All');
   });
+  it('can add a task to the inbox', function () {
+    const input = $('#add-item-input');
+    input.setValue('Task in inbox @tag');
+    $('#add-tag').click();
+    input.addValue('tag2');
+    $('#add-item-submit').click();
+    const listItem = $('main').$('.md-list').$('.md-list-item');
+    const textElement = listItem.$('.md-tile-text--primary');
+    const chipContainer = listItem.$('.Item-chip-container');
+    const chips = chipContainer.$$('.md-chip');
+    expect(textElement.getText()).to.equal('Task in inbox');
+    expect(chips[0].$('span').getText()).to.equal('@tag');
+    expect(chips[1].$('span').getText()).to.equal('@tag2');
+  });
 });
