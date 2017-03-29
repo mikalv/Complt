f4652f61b973938e8ef26cbc7873ac8f690de91e
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-03-30 09:52:15 +1000

    Add tests for loginCallback action creator

--- src/common/redux/__tests__/auth.js
+++ src/common/redux/__tests__/auth.js
@@ -1,5 +1,12 @@
 import reducer, { initialState } from '../auth';
-import { login, logout } from '../actions';
+import { login, logout, loginCallback } from '../actions';
+import { LOGIN, GET_PROFILE } from '../actionTypes';
+import history from '../../../web/history';
+import logException from '../../utils/logException';
+import getTokenInfo from '../../utils/getTokenInfo';
+import mockStore from '../mockStore';
+
+jest.mock('../../utils/logException').mock('../../utils/getTokenInfo');
 
 const token = 'jdsfjnsdfjn.sdfjsdfjsdjknfsdf324324.sdfsdf23423434';
 
@@ -12,5 +19,34 @@ describe('authReducer', () => {
   });
   it('handles LOGOUT correctly', () => {
     expect(reducer(token, logout())).toEqual(initialState);
+  });
+});
+
+describe('loginCallback()', () => {
+  it('returns nothing if there is no result', () => {
+    expect(loginCallback()()).toEqual(undefined);
+    expect(logException).not.toBeCalled();
+  });
+  it('gets the profile, dispatches the correct actions and pushes to /', () => {
+    history.push('/login');
+    expect(history.location.pathname).toEqual('/login');
+    getTokenInfo.mockReturnValue(Promise.resolve({ profileStuff: true }));
+    const store = mockStore();
+    store.dispatch(loginCallback(undefined, { idToken: token }));
+    expect(store.getActions()).toEqual([{
+      type: LOGIN, token,
+    }]);
+    expect(history.location.pathname).toEqual('/');
+    return Promise.resolve().then(() => {
+      expect(store.getActions()).toEqual([{
+        type: LOGIN, token,
+      }, {
+        type: GET_PROFILE, profile: { profileStuff: true },
+      }]);
+    });
+  });
+  it('logs the error if there is one', () => {
+    loginCallback(new Error('Some Error'))();
+    expect(logException).toBeCalledWith(new Error('Some Error'));
   });
 });
