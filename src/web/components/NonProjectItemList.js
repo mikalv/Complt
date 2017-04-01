0c632964891eef7af943130821d8d099f998bcf7
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-01 13:31:00 +1000

    Allow the user to delete and move items that have parent projects

--- src/web/components/NonProjectItemList.js
+++ src/web/components/NonProjectItemList.js
@@ -4,12 +4,27 @@ import PropTypes from '../../common/PropTypes';
 
 const NonProjectItemList = props => (<ItemList
   onLeftButtonClick={i =>
-      props.completeItem(props.items[i]._id, !props.items[i].isCompleted)}
+    props.completeItem(props.items[i]._id, !props.items[i].isCompleted)}
   onItemUpdate={i => props.showUpdateItemDialog(props.items[i]._id)}
   items={props.items}
   onItemTap={(i) => {
     if (props.items[i].isProject) {
       props.history.push(`/project/${props.items[i]._id}`);
+    }
+  }}
+  canMove
+  canDeleteTask
+  canDeleteProject
+  onDelete={(i) => {
+    const item = props.items[i];
+    if (item.parent && item.parent._id) {
+      props.deleteItem(item.parent._id, item._id);
+    }
+  }}
+  onItemMove={(i) => {
+    const item = props.items[i];
+    if (item.parent && item.parent._id) {
+      props.showMoveItemDialog(item._id, item.parent._id);
     }
   }}
 />);
@@ -21,6 +36,8 @@ NonProjectItemList.propTypes = {
   }),
   completeItem: React.PropTypes.func,
   showUpdateItemDialog: React.PropTypes.func,
+  showMoveItemDialog: React.PropTypes.func,
+  deleteItem: React.PropTypes.func,
 };
 
 export default NonProjectItemList;
