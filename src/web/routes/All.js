e7d4f4a5052ef9e5635d2c1281aca084957341f0
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-01 12:52:55 +1000

    Display an item's parent project on /all

--- src/web/routes/All.js
+++ src/web/routes/All.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import NonProjectItemList from '../components/NonProjectItemList';
 import mapDispatchToProps from '../../common/utils/mapDispatchToProps';
 import getFilteredItems from '../../common/utils/getFilteredItems';
+import { getParents } from '../../common/utils/parents';
 
 export function mapStateToProps(state) {
-  return { items: getFilteredItems(state.items.filter(({ _id }) => _id !== 'inbox' && _id !== 'root'), state.itemsToShow) };
+  const items = getParents(state.items);
+  return { items: getFilteredItems(items.filter(({ _id }) => _id !== 'inbox' && _id !== 'root'), state.itemsToShow) };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(NonProjectItemList);
