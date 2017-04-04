d08c78f4a59e5a9e1d7ceab38b07645b9300040e
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-04 16:24:00 +1000

    Fix incorrect reference to window.location.origin

--- src/web/index.js
+++ src/web/index.js
@@ -7,7 +7,7 @@ import './index.scss';
 
 if (!process.env.REACT_APP_ELECTRON) offlinePlugin.install();
 
-if (!window.origin.includes('localhost')) {
+if (!window.location.origin.includes('localhost')) {
   /* eslint-disable */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
