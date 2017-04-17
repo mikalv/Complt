0dbc20791860fa169c2bea22a906a17e506b33ad
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-17 10:27:55 +1000

    Fix tests

--- src/common/redux/__tests__/dialogs.js
+++ src/common/redux/__tests__/dialogs.js
@@ -62,37 +62,46 @@ describe('dialogsReducer', () => {
 });
 
 describe('handleUpdateItem action creator', () => {
-  it('dispatches HIDE_UPDATE_ITEM_DIALOG correctly', () => {
+  it('dispatches HIDE_UPDATE_ITEM_DIALOG correctly', (done) => {
     const store = mockStore();
     store.dispatch(handleUpdateItem('some task with a @tag', { isProject: false }));
-    expect(store.getActions()[1]).toEqual({ type: HIDE_UPDATE_ITEM_DIALOG });
+    setTimeout(() => {
+      expect(store.getActions()[1]).toEqual({ type: HIDE_UPDATE_ITEM_DIALOG });
+      done();
+    }, 100);
   });
-  it('dispatches UPDATE_ITEM correctly with a task', () => {
+  it('dispatches UPDATE_ITEM correctly with a task', (done) => {
     const store = mockStore();
     store.dispatch(handleUpdateItem('some task with a @tag', { isProject: false, name: 'some old name', tags: ['@someOtherTag'] }));
-    expect(store.getActions()[0]).toEqual({
-      type: UPDATE_ITEM,
-      item: {
-        isProject: false,
-        name: 'some task with a',
-        tags: ['@tag'],
-        dates: [],
-      },
-    });
+    setTimeout(() => {
+      expect(store.getActions()[0]).toEqual({
+        type: UPDATE_ITEM,
+        item: {
+          isProject: false,
+          name: 'some task with a',
+          tags: ['@tag'],
+          dates: [],
+        },
+      });
+      done();
+    }, 100);
   });
-  it('dispatches UPDATE_ITEM correctly with a project', () => {
+  it('dispatches UPDATE_ITEM correctly with a project', (done) => {
     const store = mockStore();
     store.dispatch(handleUpdateItem('some project', { isProject: true, name: 'some old project name', children: ['someOtherTask', 'oneMoreTask'] }));
-    expect(store.getActions()[0]).toEqual({
-      type: UPDATE_ITEM,
-      item: {
-        isProject: true,
-        name: 'some project',
-        children: ['someOtherTask', 'oneMoreTask'],
-        dates: [],
-        tags: [],
-      },
-    });
+    setTimeout(() => {
+      expect(store.getActions()[0]).toEqual({
+        type: UPDATE_ITEM,
+        item: {
+          isProject: true,
+          name: 'some project',
+          children: ['someOtherTask', 'oneMoreTask'],
+          dates: [],
+          tags: [],
+        },
+      });
+      done();
+    }, 100);
   });
 });
 
