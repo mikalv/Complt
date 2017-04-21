c50e23038ce14e1d006945a4e4b8d1d79ab35301
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-21 17:44:28 +1000

    Stop using react-hot-loader/babel

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -140,7 +140,6 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['es2015', 'react', 'stage-3', 'stage-0'],
-          plugins: ['react-hot-loader/babel'],
           babelrc: false,
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
