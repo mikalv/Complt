ae555fc3357d814edf1dad0f69faf1a310198afe
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-23 16:33:44 +1000

    Create Chip component

--- src/web/components/Item.js
+++ src/web/components/Item.js
@@ -6,14 +6,16 @@ import Delete from 'react-icons/lib/md/delete';
 import Create from 'react-icons/lib/md/create';
 import MoreVert from 'react-icons/lib/md/more-vert';
 import DragHandle from 'react-icons/lib/md/drag-handle';
-import Chip from 'react-md/lib/Chips/Chip';
 import SortableHandle from 'react-sortable-hoc/src/SortableHandle';
 import IconButton from './IconButton';
+import Chip from './Chip';
 import getNextDueDate from '../../common/utils/getNextDueDate';
 import colors from '../../common/colors';
 import './Item.css';
 
-const Handle = SortableHandle(() => <IconButton className="IconButton-margin"><DragHandle /></IconButton>);
+const Handle = SortableHandle(() => (
+  <IconButton className="IconButton-margin"><DragHandle /></IconButton>
+));
 
 const stopPropagation = (callback) => {
   if (typeof callback === 'function') {
@@ -42,43 +44,87 @@ const getFormattedDate = (rawDate) => {
   return date.toLocaleString();
 };
 
-const Item = ({
-  item = {},
-  onLeftButtonClick,
-  onDelete,
-  canDelete,
-  canMove,
-  onItemTap,
-  onItemUpdate,
-  onItemMove,
-  canSort,
-}) => (
-  <li
-    className="flex row Item md-text"
-  >
+const Item = (
+  {
+    item = {},
+    onLeftButtonClick,
+    onDelete,
+    canDelete,
+    canMove,
+    onItemTap,
+    onItemUpdate,
+    onItemMove,
+    canSort,
+  },
+) => (
+  <li className="flex row Item">
     <div className="Item-left">
-      <IconButton className="IconButton-margin" onClick={stopPropagation(onLeftButtonClick)}>{item.isProject === true ?
-        <Assignment color={item.isCompleted ? colors.completedItem : 'rgba(255, 255, 255, 0.7)'} /> :
-        <Done color={item.isCompleted ? colors.completedItem : 'rgba(255, 255, 255, 0.7)'} />}</IconButton>
+      <IconButton className="IconButton-margin" onClick={stopPropagation(onLeftButtonClick)}>
+        {item.isProject === true
+          ? <Assignment
+            color={item.isCompleted ? colors.completedItem : 'rgba(255, 255, 255, 0.7)'}
+          />
+          : <Done color={item.isCompleted ? colors.completedItem : 'rgba(255, 255, 255, 0.7)'} />}
+      </IconButton>
     </div>
     <div className="Item-center">
       <span className="Item-name">{item.name}</span>
-      {(item.dates && item.dates.length !== 0) || (item.tags && item.tags.length !== 0) || item.parent || item.children ? <div className="Item-chip-container">
-        {!item.children ? undefined : <Link to={`/project/${item._id}`} className="Item-chip-link" onClick={stopPropagation(onItemTap)}><Chip className="Item-chip" label={item.children.length === 1 ? '1 Item' : `${item.children.length} Items`} /></Link>}
-        {!item.parent ? undefined : <Link to={`/project/${item.parent._id}`} className="Item-chip-link"><Chip className="Item-chip" label={getParentProjectName(item.parent)} /></Link>}
-        {!item.dates || item.dates.length === 0 ? undefined : <Chip className="Item-chip" label={`Due ${getFormattedDate(getNextDueDate(item.dates))}`} />}
-        {!item.tags ? undefined :
-          item.tags.map(
-            tag => <Link to={`/tag/${tag}`} key={tag} className="Item-chip-link"><Chip className="Item-chip" label={tag} /></Link>)}
-      </div> : undefined}
+      {(item.dates && item.dates.length !== 0) ||
+        (item.tags && item.tags.length !== 0) ||
+        item.parent ||
+        item.children
+          ? <div className="Item-chip-container">
+            {!item.children
+              ? undefined
+                : <Chip
+                  Component={Link}
+                  to={`/project/${item._id}`}
+                  onClick={stopPropagation(onItemTap)}
+                  action
+                  className="Item-chip"
+                >
+                  {item.children.length === 1 ? '1 Item' : `${item.children.length} Items`}
+                </Chip>}
+            {!item.parent
+              ? undefined
+              : <Chip
+                Component={Link}
+                to={`/project/${item.parent._id}`}
+                action
+                className="Item-chip"
+              >
+                {getParentProjectName(item.parent)}
+              </Chip>}
+            {!item.dates || item.dates.length === 0
+              ? undefined
+              : <Chip className="Item-chip">
+                {`Due ${getFormattedDate(getNextDueDate(item.dates))}`}
+              </Chip>}
+            {!item.tags
+              ? undefined
+              : item.tags.map(tag => (
+                <Chip Component={Link} action to={`/tag/${tag}`} key={tag} className="Item-chip">
+                  {tag}
+                </Chip>
+                ))}
+          </div>
+        : undefined}
     </div>
     <div className="Item-right">
       {canSort ? <Handle /> : undefined}
-      <IconButton className="IconButton-margin" onClick={stopPropagation(onItemUpdate)}><Create /></IconButton>
-      {canMove ? <IconButton className="IconButton-margin" onClick={stopPropagation(onItemMove)}><MoreVert /></IconButton>
-      : undefined}
-      {canDelete ?
-        <IconButton className="IconButton-margin" onClick={stopPropagation(onDelete)}><Delete /></IconButton> : undefined}
+      <IconButton className="IconButton-margin" onClick={stopPropagation(onItemUpdate)}>
+        <Create />
+      </IconButton>
+      {canMove
+        ? <IconButton className="IconButton-margin" onClick={stopPropagation(onItemMove)}>
+          <MoreVert />
+        </IconButton>
+        : undefined}
+      {canDelete
+        ? <IconButton className="IconButton-margin" onClick={stopPropagation(onDelete)}>
+          <Delete />
+        </IconButton>
+        : undefined}
     </div>
   </li>
 );
