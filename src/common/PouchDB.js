c8cb3be8f9bf0d93ec5ddc66d1c2744769f067ab
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-04-27 19:16:26 +1000

    Remove pouchdb-adapter-websql

--- src/common/PouchDB.js
+++ src/common/PouchDB.js
@@ -1,7 +1,6 @@
 import PouchDB from 'pouchdb-core';
 import PouchDBAdapterIdb from 'pouchdb-adapter-idb';
-import PouchDBAdapterWebSql from 'pouchdb-adapter-websql';
 
-PouchDB.plugin(PouchDBAdapterIdb).plugin(PouchDBAdapterWebSql);
+PouchDB.plugin(PouchDBAdapterIdb);
 
 export default PouchDB;
