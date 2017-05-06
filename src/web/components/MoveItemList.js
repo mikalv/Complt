8ecfb98300bd24b5f0f2dcd273f6526309432308
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-05-06 20:46:22 +1000

    Make MoveItemList accessible

--- src/web/components/MoveItemList.js
+++ src/web/components/MoveItemList.js
@@ -1,12 +1,19 @@
 import { h } from 'preact';
 import List from 'preact-material-components/List';
+import LinkListItem from './LinkListItem';
+import './MoveItemList.scss';
 
 export function renderListItem(item, onChooseItem, itemToMove, key) {
   if (item == null || !item.isProject || item._id === itemToMove) return null;
   return [
-    <List.Item key={key} onClick={() => onChooseItem(item._id)}>
+    <LinkListItem
+      Component={'button'}
+      className="mdc-ripple-surface"
+      key={key}
+      onClick={() => onChooseItem(item._id)}
+    >
       {item.name || 'Projects'}
-    </List.Item>,
+    </LinkListItem>,
     ...item.children.map((itemInMap, i) =>
       renderListItem(itemInMap, onChooseItem, itemToMove, i)
     ),
