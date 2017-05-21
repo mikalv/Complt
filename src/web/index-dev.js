cbe8608064f0295822cdccdea152a013b986062d
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-05-21 12:59:14 +1000

    Record component mount and update times with the user timings api

--- src/web/index-dev.js
+++ src/web/index-dev.js
@@ -1,8 +1,56 @@
 /* eslint-disable global-require */
 import './index.scss';
-import { render } from 'preact'; // eslint-disable-line import/first
+import { render, options, Component, h } from 'preact'; // eslint-disable-line import/first
 import 'preact/devtools'; // eslint-disable-line import/first
 import './index.css';
+
+const getDisplayName = component =>
+  component.displayName ||
+  component.name ||
+  (typeof component === 'string' ? component : 'Component');
+
+const wrapPerf = (WrappedComponent, displayName) =>
+  class Perf extends Component {
+    displayName = displayName;
+    componentWillMount() {
+      performance.mark(`${this.displayName} (willMount)`);
+    }
+    componentDidMount() {
+      performance.mark(`${this.displayName} (didMount)`);
+      performance.measure(
+        `${this.displayName} (mount)`,
+        `${this.displayName} (willMount)`,
+        `${this.displayName} (didMount)`
+      );
+    }
+    componentWillUpdate() {
+      performance.mark(`${this.displayName} (willUpdate)`);
+    }
+    componentDidUpdate() {
+      performance.mark(`${this.displayName} (didUpdate)`);
+      performance.measure(
+        ` ${this.displayName} (update)`,
+        `${this.displayName} (willUpdate)`,
+        `${this.displayName} (didUpdate)`
+      );
+    }
+    render(props) {
+      return h(WrappedComponent, { ...props, __perf: true });
+    }
+  };
+
+options.vnode = component => {
+  if (
+    component &&
+    component.nodeName &&
+    typeof component.nodeName === 'function' &&
+    !(component.attributes && component.attributes.__perf === true) &&
+    window.performance
+  ) {
+    const displayName = getDisplayName(component.nodeName);
+    component.nodeName = wrapPerf(component.nodeName, displayName); // eslint-disable-line no-param-reassign
+  }
+};
 
 window.ga = () => {};
 
